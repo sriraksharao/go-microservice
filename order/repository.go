c9603f9575402a0a196cb08caf92ab5bc485f30b
sriraksharao <raosri1611@gmail.com>
2025-06-27 00:46:26 -0700

    catalog microservice so far

--- order/repository.go
+++ order/repository.go
@@ -1,1 +1,134 @@
 package order
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/lib/pq"
+)
+
+type Repository interface {
+	Close()
+	PutOrder(ctx context.Context, o Order) error
+	GetOrdersForAccount(ctx context.Context, accountID string) ([]Order, error)
+}
+
+type postgresRepository struct {
+	db *sql.DB
+}
+
+func NewPostgresRepository(url string) (Repository, error) {
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return &postgresRepository{db}, nil
+}
+
+func (r *postgresRepository) Close() {
+	r.db.Close()
+}
+
+func (r *postgresRepository) PutOrder(ctx context.Context, o Order) (err error) {
+	tx, err := r.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+			return
+		}
+		err = tx.Commit()
+	}()
+	tx.ExecContext(
+		ctx,
+		"Insert into orders(id, created_at, account_id, total_price) values($1, $2, $3, $4)",
+		o.ID, o.CreatedAt, o.AccountID, o.TotalPrice,
+	)
+	if err != nil {
+		return
+	}
+
+	stmt, _ := tx.PrepareContext(ctx, pq.CopyIn("order_products", "oder_id", "product_id", "quantity"))
+	for _, p := range o.Products {
+		stmt.ExecContext(ctx, o.ID, p.ID, p.Quantity)
+		if err != nil {
+			return
+		}
+	}
+	_, err = stmt.ExecContext(ctx)
+	if err != nil {
+		return
+	}
+	stmt.Close()
+	return
+}
+
+func (r *postgresRepository) GetOrdersForAccount(ctx context.Context, accountID string) ([]Order, error) {
+	rows, err := r.db.QueryContext(
+		ctx,
+		`select
+		o.id,o.created_at, o.account_id, o.total_price::money::numeric::float8,
+		op.product_id, op.quantity
+		from orders o join order_products op on (o.id=op.order_id)
+		where o.account_id=$1,
+		order by o.id`, accountID,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	orders := []Order{}
+	order := &Order{}
+	lastOrder := &Order{}
+	orderedProduct := &OrderedProduct{}
+	products := []OrderedProduct{}
+	for rows.Next() {
+		if err = rows.Scan(
+			&order.ID,
+			&order.CreatedAt,
+			&order.AccountID,
+			&order.TotalPrice,
+			&orderedProduct.ID,
+			&orderedProduct.Quantity,
+		); err != nil {
+			return nil, err
+		}
+		if lastOrder.ID != "" && lastOrder.ID != order.ID {
+			newOrder := Order{
+				ID:         lastOrder.ID,
+				AccountID:  lastOrder.AccountID,
+				CreatedAt:  lastOrder.CreatedAt,
+				TotalPrice: lastOrder.TotalPrice,
+				Products:   lastOrder.Products,
+			}
+			orders = append(orders, newOrder)
+			products = []OrderedProduct{}
+
+		}
+		products = append(products, OrderedProduct{
+			ID:       orderedProduct.ID,
+			Quantity: orderedProduct.Quantity,
+		})
+		*lastOrder = *order
+	}
+	if lastOrder != nil {
+		newOrder := Order{
+			ID:         lastOrder.AccountID,
+			AccountID:  lastOrder.AccountID,
+			CreatedAt:  lastOrder.CreatedAt,
+			TotalPrice: lastOrder.TotalPrice,
+			Products:   lastOrder.Products,
+		}
+		orders = append(orders, newOrder)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return orders, nil
+}
